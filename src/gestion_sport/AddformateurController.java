94272f30ff50a82cbdae34089f23822ec2ee3aa2
youssef <youssefelgoumri11@gmail.com>
2021-12-07 14:47:23 +0000

    classes

---
+++ src/gestion_sport/AddformateurController.java
@@ -0,0 +1,40 @@
+
+package gestion_sport;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddformateurController implements Initializable {
+
+    @FXML
+    private Button addformateur;
+    @FXML
+    private TextField nf;
+    @FXML
+    private TextField pf;
+    @FXML
+    private TextField ef;
+    @FXML
+    private TextField af;
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+    }    
+    
+    public void vider(){
+    
+        nf.setText("");
+         pf.setText("");
+          ef.setText("");
+           af.setText("");
+    
+    }
+    
+    
+}
